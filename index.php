eced97c49ec421caf05821a63f63686fdda47405
Eric <ericventuri26@gmail.com>
2024-06-23 21:19:33 -0300

    Criando ideias iniciais

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Corte de arvore</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Corte de lenha</h1>
+    </header>
+   
+    <main>
+        <div class="wrapper">
+            
+        <div class="arvore1">
+            <p>Arvore 1</p>
+        </div>
+
+        <div class="arvore2">
+        <p>Arvore 2</p>
+        </div>
+
+        <div class="arvore3">
+        <p>Arvore 3</p>
+        </div>
+
+        <div class="arvore4">
+        <p>Arvore 4</p>
+        </div>
+
+        </div>
+    </main>
+</body>
+</html>
